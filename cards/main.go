e38ddb7bd444a8e9da10cca6a0323ec5c29a91da
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-14 00:39:33 +0300

    using underscore _ (blank identifier) to ignore a not used var

--- cards/main.go
+++ cards/main.go
@@ -13,6 +13,10 @@ func main() {
 		fmt.Println(card, i)
 	}
 
+	// using _ to ignore the index because in golang you can't make a variable without using it
+	for _, card := range cards {
+		fmt.Println("Card:", card)
+	}
 }
 
 func newCard() string {
