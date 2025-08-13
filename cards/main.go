0eb4cb9fddcbfe5199767ede1c6356dbacc06486
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-14 00:30:26 +0300

    Using slices and arrays and for loops

--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+
+	// using append makes a new slice then reassigns it to cards
+	cards = append(cards, "King of Spades")
+
+	// using a for loop first "paramater" is the index, second is the value , and use range to iterate over the slice
+	for i, card := range cards {
+		fmt.Println(card, i)
+	}
+
 }
 
 func newCard() string {
