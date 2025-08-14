cb1aaa68ced684f2fadcfdda83b715724ab62a09
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-14 20:38:54 +0300

    iterating over maps

--- maps/main.go
+++ maps/main.go
@@ -6,10 +6,15 @@ func main() {
 	colors := map[string]float64{
 		"Youssif": 99.6,
 		"Mo'men":  16.6,
+		"Mohamed": 13.3,
 	}
 
-	fmt.Println(colors)
-	delete(colors, "Mo'men")
-	fmt.Println(colors)
+	printMap(colors)
 
+}
+
+func printMap(c map[string]float64) {
+	for n, g := range c {
+		fmt.Println("Name :", n, "Grade :", g)
+	}
 }
