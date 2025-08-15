65d4e0df2f790c080085442a692b059b218f5954
Youssifahmed12 <youssefbro12397@gmail.com>
2025-08-15 15:48:32 +0300

    basic concurrent program

--- channels/main.go
+++ channels/main.go
@@ -11,23 +11,30 @@ func main() {
 		"http://facebook.com",
 		"http://youtube.com",
 	}
-
+	c := make(chan string)
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(l string) {
+func checkLink(l string, c chan string) {
 	resp, err := http.Get(l)
 	if err != nil {
 		fmt.Println(l, err)
+		c <- "Error Occured"
 		return
 	}
 	if resp.StatusCode != 200 {
 		fmt.Println(l, "something wrong happened")
+		c <- "definitely something wrong"
 		return
 	}
 
 	fmt.Println(l, "is available")
+	c <- "all perfect"
 
 }
